f7e66fb52ca224569e36617bbc98fc011e380308
万雨桑 <wysang@moyusangdeMacBook-Pro.local>
2019-02-22 20:03:22 -0800

    event dashboard and eventform

---
+++ src/features/event/EventList/EventList.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import EventLIstItem from './EventListItem'
+import EventListItem from './EventListItem';
+
+class EventList extends Component {
+  render() {
+    const {events} = this.props;
+    return (
+      <div>
+        <h1>EventList</h1>
+        {events.map((event)=>(
+        <EventListItem key={event.id} event={event}/>
+        ))}
+      </div>
+    )
+  }
+}
+
+export default EventList
